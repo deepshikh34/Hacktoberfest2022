d3210f5227a062dab33ce9218a92df32de2a8a15
TheDarkLord100 <miracleyt404@gmail.com>
2022-10-04 22:51:07 +0530

    Calculator in

---
+++ Calculator.java
@@ -0,0 +1,49 @@
+/* This is a program for a basic calculator using Java. */
+
+import java.util.Scanner;
+class Calculator {
+  public static void main(String[] args) {
+
+    char operator;
+    Double number1, number2;
+    Scanner input = new Scanner(System.in);
+
+    // ask users to enter operator
+    System.out.println("Choose an operator: +, -, *, or /");
+    operator = input.next().charAt(0);
+    // ask users to enter numbers
+    System.out.println("Enter first number");
+    number1 = input.nextDouble();
+    System.out.println("Enter second number");
+    number2 = input.nextDouble();
+
+    switch (operator) {
+
+      // performs addition between numbers
+      case '+':
+        System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
+        break;
+
+      // performs subtraction between numbers
+      case '-':
+        System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));
+        break;
+
+      // performs multiplication between numbers
+      case '*':
+        System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));
+        break;
+
+      // performs division between numbers
+      case '/':
+        System.out.println(number1 + " / " + number2 + " = " + (number1 / number2));
+        break;
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
+
+    input.close();
+  }
+}
